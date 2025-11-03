2f37494c1cae15fc6c91cc6eb202150358467338
Kuandyk Kaiyrzhan <kuandykkaiyrzhan@gmail.com>
2025-11-03 16:06:57 +0900

    chore: Initialize Spring Boot project

---
+++ src/main/java/com/couponpop/storeservice/CouponpopStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.couponpop.storeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponpopStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponpopStoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couponpop/storeservice/CouponpopStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponpop.storeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponpopStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
